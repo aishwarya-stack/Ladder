694de7d60fd590ff597091e872dea254a3c0acad
aishwarya-stack <aishwaryaashokkadam@gmail.com>
2021-09-18 17:23:40 +0530

    ADD:welcome new message

--- Practice.java
+++ Practice.java
@@ -2,6 +2,7 @@ import java.util.*;
 public class Practice {
 public static void main(String[] args)
 {
-	System.out.print("Welcome");
+	System.out.print("Welcome ");
+	System.out.print("Welcome new player");
 }
 }
