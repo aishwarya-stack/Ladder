7ab85913eae4f01c373e26625cc8a12c97a94ec2
aishwarya-stack <aishwaryaashokkadam@gmail.com>
2021-09-18 17:30:20 +0530

    ADD:dice rolled

--- Practice.java
+++ Practice.java
@@ -4,5 +4,8 @@ public static void main(String[] args)
 {
 	System.out.print("Welcome ");
 	System.out.print("Welcome new player");
+	
+	int number=(int)(Math.random()*6)+1;
+	System.out.print("number is"+number);
 }
 }
